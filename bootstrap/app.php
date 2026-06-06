396936d00a0901d081eef2c7bc870e5bf3b2d58a
aditprogaming <m.aditiya1805@gmail.com>
2026-06-06 21:18:06 +0700

    fix: resolve merge conflict in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-<<<<<<< HEAD
-=======
-use Illuminate\Http\Request;
->>>>>>> 9841157 (update website)
+ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,19 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-<<<<<<< HEAD
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-=======
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
->>>>>>> 9841157 (update website)
     })->create();
